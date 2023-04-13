f3e3cfd624356b3df4b279c6a85e23c97573bd05
给贝拉嘉晚饭 <1554208383@qq.com>
2023-04-13 19:34:57 +0800

    Update

--- src/main/java/view/DashBoardView.java
+++ src/main/java/view/DashBoardView.java
@@ -1,31 +1,26 @@
 package view;
 
+import component.ActivityCardPagination;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import service.ActivityService;
-import component.ActivityCard;
 
 
 public class DashBoardView extends Application {
 
-    private ActivityService activityService = new ActivityService();
+    private final ActivityService activityService = new ActivityService();
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage){
 
         FlowPane root = new FlowPane();
-        root.setHgap(20);
-        root.setVgap(20);
-        root.setPadding(new Insets(20));
 
-        // Add your ActivityCard components here
-        ActivityCard card1 = new ActivityCard("Activity 1", "Semester 1", "image/icon.png", 0.75);
-        ActivityCard card2 = new ActivityCard("Activity 2", "Semester 2", "image/icon.png", 1);
 
-        root.getChildren().addAll(card1, card2);
+
+
+        root.getChildren().addAll(new ActivityCardPagination());
 
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
--- src/main/java/view/ModuleInfo.java
+++ src/main/java/view/ModuleInfo.java
@@ -1,5 +1,21 @@
 package view;
 
-public class ModuleInfo {
-    
+import component.ActivityCard;
+import component.ActivityCardPagination;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class ModuleInfo extends  VBox {
+    public ModuleInfo(String activityName) {
+        setSpacing(10);
+
+        Label titleLabel = new Label("Details for " + activityName);
+        titleLabel.setStyle("-fx-font-size: 24;");
+
+        Button backButton = new Button("Go Back");
+        backButton.setOnAction(event -> getScene().setRoot(new ActivityCardPagination())); // Assuming Dashboard is your main layout class
+
+        getChildren().addAll(titleLabel, backButton);
+    }
 }

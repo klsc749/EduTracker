6d8431d91edbd02cbd7a9d71fd1b933fa1574949
Smilingpink <talkingruff@163.com>
2023-04-13 21:37:46 +0800

    finish

--- src/main/java/service/ExtraCurriculumService.java
+++ src/main/java/service/ExtraCurriculumService.java
@@ -1,5 +1,23 @@
 package service;
 
+import dao.ExtraCurriculumDao;
+import model.ExtraCurriculum;
+
+import java.util.List;
+
 public class ExtraCurriculumService {
-    
+    private ExtraCurriculumDao extraCurriculumDao = new ExtraCurriculumDao();
+
+    public void saveExtraCurriculum(ExtraCurriculum extraCurriculum) {
+        extraCurriculumDao.save(null);
+    }
+
+    public ExtraCurriculum getExtraCurriculumById(String id) {
+        return extraCurriculumDao.getExtraCurriculumById(id);
+    }
+
+    public List<ExtraCurriculum> getAllExtraCurriculums() {
+        return extraCurriculumDao.getAllExtraCurriculums();
+    }
+
 }

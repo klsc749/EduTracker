d3eaa8de41caa32c0360a5fe36d43072e02d2ea5
旺旺318 <1172350484@bupt.edu.cn>
2023-04-13 16:14:06 +0800

    add Student

---
+++ src/main/java/service/StudentService.java
@@ -0,0 +1,16 @@
+package service;
+
+import dao.StudentDao;
+import model.Student;
+
+public class StudentService {
+    private StudentDao studentDao = new StudentDao();
+
+    // public void saveStudent(Student student){
+    //    studentDao.save(student);
+    //}
+
+    public void updateEmail(String s){
+
+    }
+}

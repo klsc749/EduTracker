b8392d5b0de07cbaf024d232cf484c0927769829
给贝拉嘉晚饭 <1554208383@qq.com>
2023-04-13 14:24:02 +0800

    Update ActivityCard

--- src/main/java/component/ActivityCard.java
+++ src/main/java/component/ActivityCard.java
@@ -10,36 +10,53 @@ import javafx.scene.layout.VBox;
 
 public class ActivityCard extends VBox {
     public ActivityCard(String activityName, String semester, String imagePath, double progress) {
+        initStyle();
+
+        ImageView imageView = createImageView(imagePath);
+        ProgressIndicator progressIndicator = createProgressIndicator(imageView, progress);
+        StackPane imageProgress = createImageProgress(imageView, progressIndicator);
+        Label nameLabel = createLabel(activityName, "18", "bold", "black");
+        Label semesterLabel = createLabel(semester, "14", "normal", "gray");
+
+
+        getChildren().addAll(imageProgress, nameLabel, semesterLabel);
+    }
+
+    private void initStyle(){
         setSpacing(10);
         setAlignment(Pos.CENTER);
         setStyle("-fx-background-color: white; " +
                 "-fx-border-color: #cccccc; -fx-border-width: 2; " +
                 "-fx-border-radius: 10; -fx-background-radius: 10; " +
                 "-fx-effect: dropshadow(three-pass-box, rgba(0, 0, 0, 0.15), 10, 0, 0, 4);");
+    }
 
-        // Image
+    private ImageView createImageView(String imagePath) {
         ImageView imageView = new ImageView(new Image(imagePath));
+        
         imageView.setFitWidth(150);
         imageView.setFitHeight(150);
+        return imageView;
+    }
 
+
+    private ProgressIndicator createProgressIndicator(ImageView imageView, double progress) {
         double progressIndicatorWidth = imageView.getFitWidth() * 0.5;
-        double progressIndicatorHeight = progressIndicatorWidth;
-        // Circular progress indicator
         ProgressIndicator progressIndicator = new ProgressIndicator(progress);
-        progressIndicator.setPrefSize(progressIndicatorWidth, progressIndicatorHeight);
-        progressIndicator.setMinSize(progressIndicatorWidth, progressIndicatorHeight);
+        progressIndicator.setMinSize(progressIndicatorWidth, progressIndicatorWidth);
         progressIndicator.setStyle("-fx-progress-color: blue;");
+        return progressIndicator;
+    }
 
-        // StackPane for overlaying the image and progress indicator
+    private StackPane createImageProgress(ImageView imageView, ProgressIndicator progressIndicator) {
         StackPane imageProgress = new StackPane(imageView, progressIndicator);
         StackPane.setAlignment(progressIndicator, Pos.CENTER_RIGHT);
+        return imageProgress;
+    }
 
-        // Activity name and semester labels
-        Label nameLabel = new Label(activityName);
-        nameLabel.setStyle("-fx-font-size: 18; -fx-font-weight: bold;");
-        Label semesterLabel = new Label(semester);
-        semesterLabel.setStyle("-fx-font-size: 14; -fx-text-fill: gray;");
-
-        getChildren().addAll(imageProgress, nameLabel, semesterLabel);
+    private Label createLabel(String text, String fontSize, String fontWeight, String textColor) {
+        Label label = new Label(text);
+        label.setStyle("-fx-font-size: " + fontSize + "; -fx-font-weight: " + fontWeight + "; -fx-text-fill: " + textColor + ";");
+        return label;
     }
 }
